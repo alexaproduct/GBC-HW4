ea2e0b07f282a7cf3db6bed8f84fce0115a19145
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-18 21:42:50 +0600

    HW4-2 added

---
+++ HW4-2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: Задайте одномерный массив, 
+// заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+void FillMas (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int num = new Random().Next(0, 11);
+        arr[i] = num;
+    }
+}
+
+ void PrintMas (int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите размерность массива: ");
+int k = int.Parse(Console.ReadLine()?? "");
+int[] arr = new int[k];
+FillMas(arr);
+PrintMas(arr);
+
+int sumodd = 0;
+
+for (int i = 1; i < arr.Length; i = i+2)
+{
+    sumodd = sumodd + arr[i];
+}
+
+Console.WriteLine(sumodd);
